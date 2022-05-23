5903360fa7aa228519276a847c865164fad66d84
miwied <derpixed@gmail.com>
2022-05-23 18:23:48 +0200

    included std::list, removed unused dependencies, added page logic

--- list/list.cpp
+++ list/list.cpp
@@ -1,52 +1,145 @@
 #include <iostream>
-#include <cstdlib>
-#include <vector>
+#include <list>
 #include <string>
 #include <windows.h>
 using namespace std;
 
 HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
 short defaultTextColor = 7;
 
-
-class List
+class CustomizableList
 {
-    private:
-        int rows = 0;
-        string rowContent[8];
-    public:
-        void SetRows(int r)
+private:
+    list<string> list;
+    double rowRenderLimit = 4;
+    int startingRow = 0;
+    int page = 1;
+    int rowIndex = 1;
+    void ResetParams()
+    {
+        startingRow = 0;
+        page = 1;
+        rowIndex = 1;
+    }
+public:
+    int MaxPageNr()
+    {
+        double res = list.size() / rowRenderLimit;
+        if (res <= 0)
         {
-            rows = r;
+            res = 1;
         }
-        int GetRows()
+        return ceil(res);
+    }
+    void SetPage(int p)
+    {
+        page = p;
+    }
+    int GetPage()
+    {
+        return page;
+    }
+    void SetRowIndex(int r)
+    {
+        rowIndex = r;
+    }
+    int GetRowIndex()
+    {
+        return rowIndex;
+    }
+    void SetStartingRow(int r)
+    {
+        startingRow = r;
+    }
+    int GetStartingRow()
+    {
+        return startingRow;
+    }
+    void SetRowRenderLimit(int r)
+    {
+        rowRenderLimit = r;
+    }
+    int GetRowRenderLimit()
+    {
+        return rowRenderLimit;
+    }
+    void SetDemoData()
+    {
+        list.push_back("r1 red");
+        list.push_back("r2 green");
+        list.push_back("r3 blue");
+        list.push_back("r4 yellow");
+        list.push_back("r5 brown");
+        list.push_back("r6 purple");
+        list.push_back("r7 blue");
+        list.push_back("r8 yellow");
+        list.push_back("r9 brown");
+        list.push_back("r10 purple");
+        list.push_back("r11 pink");
+    }
+    void SetRowContent(int rowNr, string content)
+    {
+        std::list<std::string>::iterator iter = list.begin();
+        advance(iter, (rowNr - 1));
+        if ((rowNr - 1) <= list.size())
         {
-            return rows;
+            /* list.erase(iter);
+             list.insert(iter, content);*/
+             //list.assign(rowNr, content);
         }
-        void SetRowContent(int rowNr, string content)
+    }
+    void ClearRowContent()
+    {
+        ResetParams();
+        list.clear();
+    }
+    void DrawList()
+    {
+        // default row render limit
+        if (rowRenderLimit == 0)
         {
-            rowContent[rowNr-1] = content;
+            rowRenderLimit = 5;
         }
-        void ClearRowContent()
+
+        cout << "/\\ " << page << "/" << to_string(MaxPageNr()) << endl;
+
+        auto row = list.begin();
+        if (startingRow <= list.size())
         {
-            for (int i = 0; i < rows; i++)
+            advance(row, startingRow);
+        }
+
+        for (int i = 0; i < rowRenderLimit; i++)
+        {
+            if (row == list.end() || list.empty())
             {
-                rowContent[i] = "";
+                cout << "R" << rowIndex << ": " << "---" << endl;
             }
-        }
-        void DrawList()
-        {
-            cout << "/\\" << endl;
-            for (int i = 1; i < (rows + 1); i++)
+            else
             {
-                cout << "R" << i << ": " << rowContent[i-1] << endl;
+                const string& ref = *row;
+                string copy = *row;
+                cout << "R" << rowIndex << ": " << ref << endl;
+                row++;
             }
-            cout << "\\/" << endl;
+            rowIndex++;
         }
+
+        cout << "\\/" << endl;
+    }
+    void AddRow(string content)
+    {
+        ResetParams();
+        list.push_back(content);
+    }
+    void setRowRenderLimit(int rows)
+    {
+        ResetParams();
+        rowRenderLimit = rows;
+    }
 };
 
-List list;
+CustomizableList firstDemoList;
 
 string commands[9] =
 {
@@ -56,9 +149,9 @@ string commands[9] =
     "down",
     "draw",
     "clear",
-    "setRows",
-    "setContent",
-    "create"
+    "setRowView",
+    "addRow",
+    "setContent"
 };
 
 enum commandsMapping
@@ -69,14 +162,15 @@ enum commandsMapping
     down,
     draw,
     clear,
-    setRows,
-    setContent,
-    create
+    setRowView,
+    addRow,
+    setContent
 };
 
 void command()
 {
     string inputCommand = "";
+    SetConsoleTextAttribute(hConsole, defaultTextColor);
     cout << "please type in your command:" << endl;
     SetConsoleTextAttribute(hConsole, 3);
     cin >> inputCommand;
@@ -86,29 +180,67 @@ void command()
 
     if (inputCommand == commands[up])
     {
+        int currentStartingRow = firstDemoList.GetStartingRow();
+        int currentRenderLimit = firstDemoList.GetRowRenderLimit();
+        int currentPage = firstDemoList.GetPage();
+        if (currentPage > 1)
+        {
+            firstDemoList.SetStartingRow(currentStartingRow - currentRenderLimit);
+            firstDemoList.SetRowIndex(currentStartingRow - currentRenderLimit + 1);
+            firstDemoList.SetPage(currentPage - 1);
+        }
+        if (currentPage == 1)
+        {
+            firstDemoList.SetRowIndex(1);
+        }
+        firstDemoList.DrawList();
         return;
     }
     if (inputCommand == commands[down])
     {
+        int maxPage = firstDemoList.MaxPageNr();
+        int currentStartingRow = firstDemoList.GetStartingRow();
+        int currentRenderLimit = firstDemoList.GetRowRenderLimit();
+        int currentPage = firstDemoList.GetPage();
+        if (currentPage < maxPage)
+        {
+            firstDemoList.SetStartingRow(currentStartingRow + currentRenderLimit);
+            firstDemoList.SetRowIndex(currentStartingRow + currentRenderLimit + 1);
+            firstDemoList.SetPage(currentPage + 1);
+        }
+        if (currentPage == maxPage)
+        {
+            int lastStartingRow = (((maxPage - 1) * currentRenderLimit) + 1);
+            firstDemoList.SetRowIndex(lastStartingRow);
+        }
+        firstDemoList.DrawList();
         return;
     }
     if (inputCommand == commands[draw])
     {
-        list.DrawList();
+        firstDemoList.DrawList();
         return;
     }
     if (inputCommand == commands[clear])
     {
-        list.ClearRowContent();
+        firstDemoList.ClearRowContent();
         return;
     }
-    if (inputCommand == commands[setRows])
+    if (inputCommand == commands[setRowView])
     {
         int numberofrows;
         cout << "number of rows: ";
         cin >> numberofrows;
-        list.SetRows(numberofrows);
-        list.ClearRowContent();
+        firstDemoList.setRowRenderLimit(numberofrows);
+        return;
+    }
+    if (inputCommand == commands[addRow])
+    {
+        string content;
+        cout << "content: ";
+        cin.ignore();
+        getline(cin, content);
+        firstDemoList.AddRow(content);
         return;
     }
     if (inputCommand == commands[setContent])
@@ -118,17 +250,16 @@ void command()
         cout << "row number: ";
         cin >> row;
         cout << "content: ";
-        /*cin >> content;*/
         cin.ignore();
         getline(cin, content);
-        list.SetRowContent(row, content);
+        firstDemoList.SetRowContent(row, content);
         return;
     }
     if (inputCommand == commands[help])
     {
         cout << "here are all valid commands:" << endl;
         SetConsoleTextAttribute(hConsole, 14);
-        for (int i = 0; i < ((sizeof(commands)/sizeof(commands[0]))); i++)
+        for (int i = 0; i < ((sizeof(commands) / sizeof(commands[0]))); i++)
         {
             cout << commands[i] << endl;
         }
@@ -143,22 +274,15 @@ void command()
         exit(1);
         return;
     }
-    if (inputCommand == commands[create])
-    {
-        return;
-    }
+    //since no if-statement was correct its a invalid command
     SetConsoleTextAttribute(hConsole, 12);
     cout << "'" << inputCommand << "' " << "is a invalid command" << endl;
     SetConsoleTextAttribute(hConsole, defaultTextColor);
 }
 
-int pause;
-
 int main()
 {
-    SetConsoleTextAttribute(hConsole, defaultTextColor);
-    list.SetRows(8);
-    list.SetRowContent(5, "Test");
+    firstDemoList.SetDemoData();
 
     while (true)
     {
